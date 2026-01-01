c6dc3d4be9d8d014539038cc478f06363ca4b29a
smit-debian <smitdebian@gmail.com>
2026-01-01 12:32:01 +0545

    Organized

--- raylib_test/src/window.cpp
+++ raylib_test/src/window.cpp
@@ -2,13 +2,13 @@
 #include "raylib.h"
 
 Window::Window( int width, int height, const std::string& title, int fps )
-	: width( width ), height( height ), title( title ), fps( fps )
+        : width( width ), height( height ), title( title ), fps( fps )
 {
-	InitWindow( width, height, title.c_str() );
-	SetTargetFPS( fps );
+        InitWindow( width, height, title.c_str() );
+        SetTargetFPS( fps );
 }
 
 Window::~Window()
 {
-	CloseWindow();
+        CloseWindow();
 }
