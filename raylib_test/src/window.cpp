3a5d7625b59ec970edb4546cbcbe1800fc61486b
smit-debian <smitdebian@gmail.com>
2026-01-01 12:23:21 +0545

    creation

---
+++ raylib_test/src/window.cpp
@@ -0,0 +1,14 @@
+#include "window.hpp"
+#include "raylib.h"
+
+Window::Window( int width, int height, const std::string& title, int fps )
+	: width( width ), height( height ), title( title ), fps( fps )
+{
+	InitWindow( width, height, title.c_str() );
+	SetTargetFPS( fps );
+}
+
+Window::~Window()
+{
+	CloseWindow();
+}
